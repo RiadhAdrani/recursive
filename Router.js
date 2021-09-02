18775ace992ed6173f0e22ee3306bde651d2e34c
adrani_laptop <riadh_adrani@hotmail.fr>
2021-09-02 13:57:38 +0100

    Added a basic routing system

---
+++ Router.js
@@ -0,0 +1,58 @@
+class Router {
+     constructor(routes) {
+          this.routes = [];
+          this.current = null;
+          if (Array.isArray(routes)) {
+               routes.forEach((r) => {
+                    if (!r.name) throw `Invalid name`;
+                    if (!r.component) throw `Invalid component`;
+               });
+               this.routes = routes;
+          }
+          this.current = routes[0];
+          this.stack = [routes[0]];
+
+          window.addEventListener("popstate", (event) => {
+               if (this.stack.length === 1) {
+                    this.current = routes[0];
+                    window.vDOM.update();
+                    return;
+               }
+               const x = this.stack.pop();
+               console.log(x);
+               routes.forEach((r) => {
+                    if (x.name === r.name) {
+                         this.current = r;
+                         window.vDOM.update();
+                         return;
+                    }
+               });
+          });
+     }
+
+     static Route = ({ name, component, title }) => {
+          return {
+               name,
+               title,
+               component,
+          };
+     };
+
+     render() {
+          return this.current.component();
+     }
+
+     goTo(name) {
+          this.routes.forEach((r) => {
+               if (name === r.name && name !== this.current.name) {
+                    history.pushState({}, this.current.title, `${name}`);
+                    this.stack.push(this.current);
+                    this.current = r;
+                    window.vDOM.update();
+                    return;
+               }
+          });
+     }
+}
+
+export default Router;
