ae30041e9ecadb725a19840c2436998c18aaec29
RiadhAdrani <riadh_adrani@hotmail.fr>
2022-08-07 19:50:34 +0100

    Added path and anchor splitter function with tests

---
+++ packages/router/src/stripPathAndAnchor.js
@@ -0,0 +1,28 @@
+const { ROUTER_ANCHOR_REG_EXP } = require("../../constants");
+
+/**
+ * Divide a destination into path and anchor.
+ * @param {String} destination
+ * @return {[String,String]} result
+ */
+function stripPathAndAnchor(destination) {
+    if (typeof destination !== "string") return ["", ""];
+
+    const regEx = ROUTER_ANCHOR_REG_EXP;
+
+    const result = regEx.exec(destination);
+
+    let path = "";
+    let anchor = "";
+
+    if (result) {
+        path = destination.substring(0, result.index);
+        anchor = destination.substring(result.index);
+    } else {
+        path = destination;
+    }
+
+    return [path, anchor];
+}
+
+module.exports = stripPathAndAnchor;
---
+++ packages/router/tests/stripPathAndAnchor.test.js
@@ -0,0 +1,17 @@
+const stripPathAndAnchor = require("../src/stripPathAndAnchor");
+
+it.each([
+    ["/", ["/", ""]],
+    ["/my-route", ["/my-route", ""]],
+    ["/#123", ["/", "#123"]],
+    ["/my-route#anchor#123", ["/my-route", "#anchor#123"]],
+    ["/my-route#123", ["/my-route", "#123"]],
+    ["/route/nested#123", ["/route/nested", "#123"]],
+    ["/route/nested/nested#123", ["/route/nested/nested", "#123"]],
+    [undefined, ["", ""]],
+    [null, ["", ""]],
+    [{}, ["", ""]],
+    [[], ["", ""]],
+])("should split path and anchor correctly case : '%s' => %s", (input, expected) => {
+    expect(stripPathAndAnchor(input)).toStrictEqual(expected);
+});
