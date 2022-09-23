b8aacd289ff1cb9a87cf4952f087650e4e554e29
riadh-adrani <riadh_adrani@hotmail.fr>
2022-09-23 11:55:05 +0100

    change: package.json is now pointing into index.d.ts for typing

---
+++ index.d.ts
@@ -0,0 +1,16 @@
+import { RecursiveApp } from "./packages/app";
+import { RecursiveRouter } from "./packages/router";
+import { RecursiveRenderer, createElement } from "./packages/renderer";
+import { RecursiveState } from "./packages/state";
+import { RecursiveOrchestrator } from "./packages/orchestrator";
+import { RecursiveConsole } from "./packages/console";
+
+export {
+    RecursiveApp,
+    RecursiveRouter,
+    RecursiveRenderer,
+    createElement,
+    RecursiveState,
+    RecursiveOrchestrator,
+    RecursiveConsole,
+};
