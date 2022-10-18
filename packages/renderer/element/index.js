ef60204af6f9a34a4713141d1a1027cdbed6882d
RiadhAdrani <riadh_adrani@hotmail.fr>
2022-10-18 20:25:58 +0200

    feat: elementType could be overriden by props

--- packages/renderer/element/index.js
+++ packages/renderer/element/index.js
@@ -2,8 +2,8 @@ const { RECURSIVE_ELEMENT_SYMBOL } = require("../../constants");
 
 function createElement(elementType, props) {
     return {
-        ...props,
         elementType,
+        ...props,
         $$_RecursiveSymbol: RECURSIVE_ELEMENT_SYMBOL,
     };
 }
