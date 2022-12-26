c7d03f2b5b11402b79bb95cba4e09750a762c865
RiadhAdrani <riadh_adrani@hotmail.fr>
2022-12-26 15:52:35 +0100

    fix: export typos

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 export { RecursiveOrchestrator } from "./packages/orchestrator";
-export { RecursiveRenderer, create } from "./packages/renderer";
+export { RecursiveRenderer, createElement } from "./packages/renderer";
 export { RecursiveRouter, createRoute } from "./packages/router";
 export { RecursiveConsole } from "./packages/console";
 export { RecursiveApp } from "./packages/app";
